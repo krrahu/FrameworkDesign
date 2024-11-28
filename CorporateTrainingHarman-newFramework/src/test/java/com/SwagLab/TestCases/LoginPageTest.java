e8aeef57b301284ae9c5b37b21e28f50ac9f21c7
krrahu <rahul.baliyase91@gmail.com>
2024-11-28 21:34:31 +0530

    new framework -28-11-2024

---
+++ CorporateTrainingHarman-newFramework/src/test/java/com/SwagLab/TestCases/LoginPageTest.java
@@ -0,0 +1,26 @@
+package com.SwagLab.TestCases;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginPageTest extends BaseClass
+{
+  @Test(priority=1)
+  public void validateUrl()
+  {
+	  String actUrl=lp.getAppUrl();
+	  AssertJUnit.assertTrue(actUrl.contains(prop.getData("expUrl")));
+	  System.out.println("Url is matching!");
+  }
+  
+  @Test(priority=2)
+  public void validateLogin()
+  {
+	
+	  lp.doLogin(prop.getData("un"),prop.getData("psw")); // calling prop file & taking value of key un and psw
+	  AssertJUnit.assertTrue(lp.getAppUrl().contains(prop.getData("explp")));
+	  System.out.println("Login Success!");
+  }
+}
